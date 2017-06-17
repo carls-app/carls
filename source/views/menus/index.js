a7e7f9457ab9806872a0116e14770b7c2711bb70
Hawken Rives <hawkrives@gmail.com>
2017-06-16 23:32:29 -0500

    initial Carleton menus

--- source/views/menus/index.js
+++ source/views/menus/index.js
@@ -12,21 +12,19 @@ import {TabBarIcon} from '../components/tabbar-icon'
 import {BonAppHostedMenu} from './menu-bonapp'
 import {GitHubHostedMenu} from './menu-github'
 import {
-  CarletonCafeIndex,
-  CarletonBurtonMenuScreen,
-  CarletonLDCMenuScreen,
-  CarletonWeitzMenuScreen,
-  CarletonSaylesMenuScreen,
-} from './carleton-menus'
+  StavHallMenuView,
+  TheCageMenuView,
+  ThePauseMenuView,
+  CafeIndex,
+} from './stolaf-menus'
 // import {BonAppPickerView} from './dev-bonapp-picker'
 
-const CarletonMenuPicker = StackNavigator(
+const StOlafMenuPicker = StackNavigator(
   {
-    CarletonCafeIndex: {screen: CarletonCafeIndex},
-    CarletonBurtonMenuView: {screen: CarletonBurtonMenuScreen},
-    CarletonLDCMenuView: {screen: CarletonLDCMenuScreen},
-    CarletonWeitzMenuView: {screen: CarletonWeitzMenuScreen},
-    CarletonSaylesMenuView: {screen: CarletonSaylesMenuScreen},
+    CafeIndex: {screen: CafeIndex},
+    ThePauseMenuView: {screen: ThePauseMenuView},
+    TheCageMenuView: {screen: TheCageMenuView},
+    StavHallMenuView: {screen: StavHallMenuView},
   },
   {
     headerMode: 'none',
@@ -35,59 +33,72 @@ const CarletonMenuPicker = StackNavigator(
 
 export const MenusView = TabNavigator(
   {
-    StavHallMenuView: {
+    BurtonMenuScreen: {
       screen: ({navigation}) =>
         <BonAppHostedMenu
           navigation={navigation}
-          name="stav"
-          cafeId="261"
-          loadingMessage={[
-            'Hunting Ferndale Turkey…',
-            'Tracking wild vegan burgers…',
-            '"Cooking" some lutefisk…',
-          ]}
+          name="burton"
+          cafeId="35"
+          loadingMessage={['Searching for Schiller…']}
         />,
       navigationOptions: {
-        tabBarLabel: 'Stav Hall',
+        tabBarLabel: 'Burton',
         tabBarIcon: TabBarIcon('nutrition'),
       },
     },
 
-    TheCageMenuView: {
+    LDCMenuScreen: {
       screen: ({navigation}) =>
         <BonAppHostedMenu
           navigation={navigation}
-          name="cage"
-          cafeId="262"
-          ignoreProvidedMenus={true}
-          loadingMessage={[
-            'Checking for vegan cookies…',
-            'Serving up some shakes…',
-          ]}
+          name="ldc"
+          cafeId="36"
+          loadingMessage={['Tracking down empty seats…']}
         />,
       navigationOptions: {
-        tabBarLabel: 'The Cage',
+        tabBarLabel: 'LDC',
         tabBarIcon: TabBarIcon('cafe'),
       },
     },
 
-    ThePauseMenuView: {
+    WeitzMenuScreen: {
       screen: ({navigation}) =>
-        <GitHubHostedMenu
+        <BonAppHostedMenu
           navigation={navigation}
-          name="pause"
-          loadingMessage={['Mixing up a shake…', 'Spinning up pizzas…']}
+          name="weitz"
+          cafeId="458"
+          loadingMessage={[
+            'Observing the artwork…',
+            'Previewing performances…',
+          ]}
         />,
       navigationOptions: {
-        tabBarLabel: 'The Pause',
+        tabBarLabel: 'Weitz Center',
         tabBarIcon: TabBarIcon('paw'),
       },
     },
 
-    CarletonMenuListView: {
-      screen: CarletonMenuPicker,
+    SaylesMenuScreen: {
+      screen: ({navigation}: TopLevelViewPropsType) =>
+        <BonAppHostedMenu
+          navigation={navigation}
+          name="sayles"
+          cafeId="34"
+          loadingMessage={[
+            'Engaging in people-watching…',
+            'Checking the mail…',
+          ]}
+        />,
       navigationOptions: {
-        title: 'Carleton',
+        title: 'Sayles Hill',
+        tabBarIcon: TabBarIcon('menu'),
+      },
+    },
+
+    StOlafMenuListView: {
+      screen: StOlafMenuPicker,
+      navigationOptions: {
+        title: 'St. Olaf',
         tabBarIcon: TabBarIcon('menu'),
       },
     },
---
+++ source/views/menus/stolaf-menus.js
@@ -0,0 +1,90 @@
+// @flow
+import React from 'react'
+import {TabBarIcon} from '../components/tabbar-icon'
+import {View, Platform, ScrollView, StyleSheet} from 'react-native'
+import type {TopLevelViewPropsType} from '../types'
+import {Row} from '../components/layout'
+import {ListRow, ListSeparator, Title} from '../components/list'
+import {BonAppHostedMenu} from './menu-bonapp'
+import {GitHubHostedMenu} from './menu-github'
+
+export const StavHallMenuView = ({navigation}) =>
+  <BonAppHostedMenu
+    navigation={navigation}
+    name="stav"
+    cafeId="261"
+    loadingMessage={[
+      'Hunting Ferndale Turkey…',
+      'Tracking wild vegan burgers…',
+      '"Cooking" some lutefisk…',
+    ]}
+  />
+StavHallMenuView.navigationOptions = {
+  tabBarLabel: 'Stav Hall',
+  tabBarIcon: TabBarIcon('nutrition'),
+}
+
+export const TheCageMenuView = ({navigation}) =>
+  <BonAppHostedMenu
+    navigation={navigation}
+    name="cage"
+    cafeId="262"
+    ignoreProvidedMenus={true}
+    loadingMessage={['Checking for vegan cookies…', 'Serving up some shakes…']}
+  />
+TheCageMenuView.navigationOptions = {
+  tabBarLabel: 'The Cage',
+  tabBarIcon: TabBarIcon('cafe'),
+}
+
+export const ThePauseMenuView = ({navigation}) =>
+  <GitHubHostedMenu
+    navigation={navigation}
+    name="pause"
+    loadingMessage={['Mixing up a shake…', 'Spinning up pizzas…']}
+  />
+ThePauseMenuView.navigationOptions = {
+  tabBarLabel: 'The Pause',
+  tabBarIcon: TabBarIcon('paw'),
+}
+
+export class CafeIndex extends React.Component {
+  props: TopLevelViewPropsType
+
+  render() {
+    const stolafCafes = [
+      {id: 'StavHallMenuView', title: 'Burton'},
+      {id: 'TheCageMenuView', title: 'LDC'},
+      {id: 'ThePauseMenuView', title: 'Weitz Center'},
+    ]
+
+    return (
+      <ScrollView style={styles.container}>
+        {stolafCafes.map((loc: {id: string, title: string}, i, collection) =>
+          <View key={i}>
+            <ListRow
+              onPress={() => this.props.navigation.navigate(loc.id)}
+              arrowPosition="center"
+            >
+              <Row alignItems="center">
+                <Title style={styles.rowText}>{loc.title}</Title>
+              </Row>
+            </ListRow>
+            {i < collection.length - 1
+              ? <ListSeparator spacing={{left: 15}} />
+              : null}
+          </View>,
+        )}
+      </ScrollView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  rowText: {
+    paddingVertical: 6,
+  },
+  container: {
+    paddingTop: Platform.OS === 'ios' ? 20 : 0,
+  },
+})
