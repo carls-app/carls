a89507dd41024bd0ac5b573c31b16b01c667c178
Hawken Rives <rives@stolaf.edu>
2017-08-27 01:17:08 -0500

    poke

--- source/views/sis/student-work-carls/detail.ios.js
+++ source/views/sis/student-work-carls/detail.ios.js
@@ -59,7 +59,7 @@ function Description({job}: {job: FullJobType}) {
 }
 
 function Links({job}: {job: FullJobType}) {
-  const links = Array.from(getUrls(job))
+  const links = Array.from(getUrls(job).values())
   return links.length
     ? <Section header="LINKS">
         {links.map(url =>
