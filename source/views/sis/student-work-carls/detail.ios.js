c10a6aa7a4870da1f8159b27d92f4756454711ad
Hawken Rives <rives@stolaf.edu>
2017-08-26 23:51:52 -0500

    add the working student-work view

---
+++ source/views/sis/student-work-carls/detail.ios.js
@@ -0,0 +1,96 @@
+// @flow
+import React from 'react'
+import {ScrollView} from 'react-native'
+import {Cell, Section, TableView} from 'react-native-tableview-simple'
+import openUrl from '../../components/open-url'
+import type {FullJobType} from './types'
+import getUrls from 'get-urls'
+import {SelectableCell} from './selectable'
+
+function Title({job}: {job: FullJobType}) {
+  return (
+    <Section header="JOB">
+      <Cell
+        cellStyle="Subtitle"
+        title={job.title}
+        detail={job.offCampus ? 'Off-Campus' : ''}
+      />
+    </Section>
+  )
+}
+
+function Information({job}: {job: FullJobType}) {
+  const department = job.department
+    ? <Cell
+        cellStyle="RightDetail"
+        title="Department"
+        detail={job.department}
+      />
+    : null
+
+  const opens = (
+    <Cell cellStyle="RightDetail" title="Date Open" detail={job.dateOpen} />
+  )
+
+  const term = job.duringTerm
+    ? <Cell cellStyle="Basic" title="Position Available During Term" />
+    : null
+
+  const brk = job.duringBreak
+    ? <Cell cellStyle="Basic" title="Position Available During Break" />
+    : null
+
+  return (
+    <Section header="INFORMATION">
+      {department}
+      {opens}
+      {term}
+      {brk}
+    </Section>
+  )
+}
+
+function Description({job}: {job: FullJobType}) {
+  return (
+    <Section header="DESCRIPTION">
+      <SelectableCell text={job.description} />
+    </Section>
+  )
+}
+
+function Links({job}: {job: FullJobType}) {
+  const links = [...getUrls(job)]
+  return links.length
+    ? <Section header="LINKS">
+        {links.map(url =>
+          <Cell
+            key={url}
+            title={url}
+            accessory="DisclosureIndicator"
+            onPress={() => openUrl(url)}
+          />,
+        )}
+      </Section>
+    : null
+}
+
+export class JobDetailView extends React.PureComponent {
+  props: {
+    job: FullJobType,
+  }
+
+  render() {
+    const {job} = this.props
+
+    return (
+      <ScrollView>
+        <TableView>
+          <Title job={job} />
+          <Information job={job} />
+          <Description job={job} />
+          <Links job={job} />
+        </TableView>
+      </ScrollView>
+    )
+  }
+}
