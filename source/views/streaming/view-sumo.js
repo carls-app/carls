b1c007e221e1c984b42eb60033eb5fbd32ea964f
Hawken Rives <hawkrives@gmail.com>
2018-02-25 23:30:58 -0600

    create new separate SUMO and Radio views

---
+++ source/views/streaming/view-sumo.js
@@ -0,0 +1,66 @@
+// @flow
+
+import * as React from 'react'
+import {TabNavigator} from '../components/tabbed-view'
+import {TabBarIcon} from '../components/tabbar-icon'
+import {ReasonCalendarView} from '../calendar/calendar-reason'
+
+export {KSTOScheduleView, KRLXScheduleView} from './radio'
+
+export const SumoTabView = TabNavigator(
+	{
+		NextWeeklyMovieView: {
+			screen: ({navigation}) => (
+				<ReasonCalendarView
+					calendarUrl="https://apps.carleton.edu/student/orgs/sumo/"
+					eventMapper={event => ({
+						...event,
+						title: event.title.replace(/^SUMO: /, ''),
+						config: {
+							...event.config,
+							endTime: false,
+						},
+					})}
+					navigation={navigation}
+					poweredBy={{
+						title: 'Powered by SUMO',
+						href: 'https://apps.carleton.edu/student/orgs/sumo/',
+					}}
+				/>
+			),
+			navigationOptions: {
+				tabBarLabel: 'Up Next',
+				tabBarIcon: TabBarIcon('film'),
+			},
+		},
+		FutureWeeklyMoviesView: {
+			screen: ({navigation}) => (
+				<ReasonCalendarView
+					calendarUrl="https://apps.carleton.edu/student/orgs/sumo/"
+					eventMapper={event => ({
+						...event,
+						title: event.title.replace(/^SUMO: /, ''),
+						config: {
+							...event.config,
+							endTime: false,
+						},
+					})}
+					navigation={navigation}
+					poweredBy={{
+						title: 'Powered by SUMO',
+						href: 'https://apps.carleton.edu/student/orgs/sumo/',
+					}}
+				/>
+			),
+			navigationOptions: {
+				tabBarLabel: 'Upcoming',
+				tabBarIcon: TabBarIcon('film'),
+			},
+		},
+	},
+	{
+		navigationOptions: {
+			title: 'SUMO',
+		},
+	},
+)
