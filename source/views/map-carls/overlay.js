fd6d7c30382335fc555d2b02644d1ed8c5554704
Hawken Rives <hawkrives@gmail.com>
2018-03-04 21:58:16 -0600

    poke at external manipulation of overlay size

--- source/views/map-carls/overlay.js
+++ source/views/map-carls/overlay.js
@@ -16,22 +16,36 @@ type Props = {
 	onSizeChange: ViewState => any,
 }
 
-type State = {
-	viewState: ViewState,
-}
-
 const screenHeight = Dimensions.get('window').height - 75
 
-export class Overlay extends React.Component<Props, State> {
-	state = {
-		viewState: this.props.size,
+export class Overlay extends React.Component<Props> {
+	componentWillReceiveProps(nextProps: Props) {
+		console.warn(nextProps.size)
+		Animated.timing(
+			this._deltaY,
+			{toValue: this.lookupPosition(nextProps.size)}
+		).start()
 	}
 
-	// _deltaY = new Animated.Value(screenHeight - 100)
+	positions = {
+		max: 40,
+		mid: screenHeight - 300,
+		min: screenHeight - 100,
+	}
+
+	lookupPosition = (size: ViewState) => {
+		return this.positions[size]
+	}
+
+	_deltaY = new Animated.Value(this.lookupPosition(this.props.size))
 
 	resizeMin = () => this.props.onSizeChange('min')
 	resizeMid = () => this.props.onSizeChange('mid')
 	resizeMax = () => this.props.onSizeChange('max')
+
+	onSnap = (index: number, id: ViewState) => {
+		this.props.onSizeChange(id)
+	}
 
 	render() {
 		const {
@@ -43,7 +57,6 @@ export class Overlay extends React.Component<Props, State> {
 
 		let style = [
 			styles.overlay,
-			// overlaySize,
 			outerStyle,
 		]
 
@@ -94,13 +107,14 @@ export class Overlay extends React.Component<Props, State> {
 
 				<Interactable.View
 					// to play with the darkening bg, uncomment the following line as well
-					//animatedValueY={this._deltaY}
+					animatedValueY={this._deltaY}
 					boundaries={{top: -300}}
-					initialPosition={{y: screenHeight - 100}}
+					initialPosition={{y: this.positions[viewState]}}
+					onSnap={this.onSnap}
 					snapPoints={[
-						{y: 40},
-						{y: screenHeight - 300},
-						{y: screenHeight - 100},
+						{id: 'max', y: this.positions.max},
+						{id: 'mid', y: this.positions.mid},
+						{id: 'min', y: this.positions.min},
 					]}
 					verticalOnly={true}
 				>
