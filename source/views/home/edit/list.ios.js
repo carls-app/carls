7a7b936cca98d207e9d67631183e287c4a25fe27
Drew Volz <volz.drew@gmail.com>
2018-03-09 20:07:00 -0700

    Prettier on list

--- source/views/home/edit/list.ios.js
+++ source/views/home/edit/list.ios.js
@@ -19,79 +19,79 @@ import {toggleViewDisabled} from '../../../flux/parts/homescreen'
 const objViews = fromPairs(allViews.map(v => [v.view, v]))
 
 const styles = StyleSheet.create({
-  contentContainer: {
-    backgroundColor: c.iosLightBackground,
-    paddingTop: 10,
-    paddingBottom: 20,
-    flexDirection: 'column',
-    alignItems: 'stretch',
-  },
+	contentContainer: {
+		backgroundColor: c.iosLightBackground,
+		paddingTop: 10,
+		paddingBottom: 20,
+		flexDirection: 'column',
+		alignItems: 'stretch',
+	},
 })
 
 type ReduxStateProps = {
-  order: string[],
-  inactiveViews: string[],
+	order: string[],
+	inactiveViews: string[],
 }
 
 type ReduxDispatchProps = {
-  onSaveOrder: (string[]) => any,
-  onToggleViewDisabled: string => any,
+	onSaveOrder: (string[]) => any,
+	onToggleViewDisabled: string => any,
 }
 
 type Props = ReduxStateProps & ReduxDispatchProps
 
 class EditHomeView extends React.PureComponent<Props> {
-  static navigationOptions = {
-    title: 'Edit Home',
-  }
+	static navigationOptions = {
+		title: 'Edit Home',
+	}
 
-  renderRow = (args: {data: ViewType, active: boolean, width: number}) => {
-    const {data, active, width} = args
-    const enabled = !this.props.inactiveViews.includes(data.view)
-    return (
-      <EditHomeRow
-        active={active}
-        data={data}
-        isEnabled={enabled}
-        onToggle={this.props.onToggleViewDisabled}
-        width={width}
-      />
-    )
-  }
+	renderRow = (args: {data: ViewType, active: boolean, width: number}) => {
+		const {data, active, width} = args
+		const enabled = !this.props.inactiveViews.includes(data.view)
+		return (
+			<EditHomeRow
+				active={active}
+				data={data}
+				isEnabled={enabled}
+				onToggle={this.props.onToggleViewDisabled}
+				width={width}
+			/>
+		)
+	}
 
-  onChangeOrder = (order: string[]) => this.props.onSaveOrder(order)
+	onChangeOrder = (order: string[]) => this.props.onSaveOrder(order)
 
-  render() {
-    return (
-      <Viewport
-        render={({width}) => (
-          <SortableList
-            contentContainerStyle={[styles.contentContainer, {width}]}
-            data={objViews}
-            onChangeOrder={this.onChangeOrder}
-            order={this.props.order}
-            renderRow={props => this.renderRow({...props, width})}
-          />
-        )}
-      />
-    )
-  }
+	render() {
+		return (
+			<Viewport
+				render={({width}) => (
+					<SortableList
+						contentContainerStyle={[styles.contentContainer, {width}]}
+						data={objViews}
+						onChangeOrder={this.onChangeOrder}
+						order={this.props.order}
+						renderRow={props => this.renderRow({...props, width})}
+					/>
+				)}
+			/>
+		)
+	}
 }
 
 function mapState(state: ReduxState): ReduxStateProps {
-  return {
-    order: state.homescreen ? state.homescreen.order : [],
-    inactiveViews: state.homescreen ? state.homescreen.inactiveViews : [],
-  }
+	return {
+		order: state.homescreen ? state.homescreen.order : [],
+		inactiveViews: state.homescreen ? state.homescreen.inactiveViews : [],
+	}
 }
 
 function mapDispatch(dispatch): ReduxDispatchProps {
-  return {
-    onSaveOrder: newOrder => dispatch(saveHomescreenOrder(newOrder)),
-    onToggleViewDisabled: view => dispatch(toggleViewDisabled(view)),
-  }
+	return {
+		onSaveOrder: newOrder => dispatch(saveHomescreenOrder(newOrder)),
+		onToggleViewDisabled: view => dispatch(toggleViewDisabled(view)),
+	}
 }
 
 export const ConnectedEditHomeView = connect(mapState, mapDispatch)(
-  EditHomeView,
+	EditHomeView,
 )
