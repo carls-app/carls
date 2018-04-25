0d5e37f74a34ed933eadfec38c09b6527dcb8d0f
Hawken Rives <hawkrives@gmail.com>
2018-04-24 23:42:09 -0500

    rebuild convo detail view to fit the video to the viewport

--- source/views/convocations/archived-detail.js
+++ source/views/convocations/archived-detail.js
@@ -1,40 +1,44 @@
 // @flow
 
-import React from 'react'
+import * as React from 'react'
 import {StyleSheet, WebView, ScrollView} from 'react-native'
-import {Title} from '../components/list'
+import glamorous from 'glamorous-native'
 import type {PodcastEpisode} from './types'
+import moment from 'moment'
 import {AllHtmlEntities} from 'html-entities'
 import qs from 'querystring'
+import {Viewport} from '../components/viewport'
 import {TextButton} from '../components/nav-buttons'
 import {openUrl} from '../components/open-url'
+
+const entities = new AllHtmlEntities()
 
 const styles = StyleSheet.create({
 	audio: {
 		width: 200,
 		height: 100,
 	},
-	video: {
-		width: 854,
-		height: 480,
-		flex: 1,
-	},
+	video: {},
 })
 
-const htmlVideo = (url: string) => `<!DOCTYPE html>
-  <style>
-    video {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-    }
-  </style>
-  <video webkit-playsinline playsinline controls>
-    <source src="${url}" type="video/mp4" />
-  </video>
-  `
+const htmlVideo = (params: string) => `<!DOCTYPE html>
+<style>
+body {
+	margin: 0;
+}
+iframe {
+	width: 100%;
+	height: 100%;
+	background-color: black;
+}
+</style>
+<iframe
+	scrolling="no"
+	frameborder="0"
+	allowfullscreen
+	src="https://apps.carleton.edu/reason_package/reason_4.0/www/scripts/media/media_iframe.php?${params}"
+></iframe>
+`
 
 type Props = {
 	navigation: {
@@ -45,6 +49,30 @@ type Props = {
 		},
 	},
 }
+
+const Title = glamorous.text({
+	fontSize: 36,
+	textAlign: 'center',
+	marginHorizontal: 18,
+	marginVertical: 10,
+})
+
+const Description = glamorous.text({
+	fontSize: 18,
+	textAlign: 'left',
+	marginHorizontal: 18,
+	marginVertical: 10,
+})
+
+const ConvoDate = glamorous(Description)({
+	textAlign: 'center',
+})
+
+const ConvoLength = glamorous(ConvoDate)({})
+
+const CONVO_WIDTH = 639
+const CONVO_HEIGHT = 360
+const RATIO = CONVO_HEIGHT / CONVO_WIDTH
 
 function getConvoUrlParams(mediaUrl: string) {
 	return mediaUrl.split('?')[1]
@@ -71,7 +99,10 @@ export class ArchivedConvocationDetailView extends React.Component<Props> {
 
 		return {
 			headerRight: (
-				<TextButton onPress={() => openUrl(link)} title="Open on carleton.edu" />
+				<TextButton
+					onPress={() => openUrl(link)}
+					title="Open on carleton.edu"
+				/>
 			),
 		}
 	}
@@ -86,15 +117,52 @@ export class ArchivedConvocationDetailView extends React.Component<Props> {
 			style = styles.video
 		}
 
+		let title = event.title.replace(/^Convocation: /, '')
+		let date = event.pubDate.format('dddd, MMMM Do YYYY')
+
+		let convoUrlParams = event.enclosure
+			? getConvoUrlParams(event.enclosure.url)
+			: ''
+
+		let convoLength = null
+		if (event.enclosure) {
+			let duration = moment.duration(parseInt(event.enclosure.length) / 100)
+
+			let hours =
+				duration.hours() === 1
+					? `${duration.hours()} hour`
+					: `${duration.hours()} hours`
+
+			let minutes =
+				duration.minutes() === 0
+					? ''
+					: duration.minutes() === 1
+						? `, ${duration.minutes()} minute`
+						: `, ${duration.minutes()} minutes`
+
+			convoLength = `${hours}${minutes}`
+		}
+
 		return (
 			<ScrollView>
-				<Title>{event.title}</Title>
+				<ConvoDate>{date}</ConvoDate>
+				<Title>{title}</Title>
+				<Description>{event.description}</Description>
 
 				{event.enclosure ? (
-					<WebView
-						source={{html: htmlVideo(event.enclosure.url)}}
-						style={style}
-					/>
+					<React.Fragment>
+						<Viewport
+							render={({width}) => (
+								<WebView
+									scrollEnabled={false}
+									source={{html: htmlVideo(convoUrlParams)}}
+									style={[style, {width: width, height: RATIO * width}]}
+								/>
+							)}
+						/>
+
+						<ConvoLength>{convoLength}</ConvoLength>
+					</React.Fragment>
 				) : null}
 			</ScrollView>
 		)
