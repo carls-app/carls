94542e02889d18a33fea489daac588ec585808ea
Hawken Rives <hawkrives@gmail.com>
2018-04-28 08:24:28 -0500

    start using the UpcomingConvocationsDetailView

--- source/views/convocations/upcoming.js
+++ source/views/convocations/upcoming.js
@@ -17,6 +17,7 @@ export class UpcomingConvocationsView extends React.PureComponent<Props> {
 		return (
 			<ReasonCalendarView
 				calendarUrl="https://apps.carleton.edu/events/convocations/"
+				detailView="UpcomingConvocationsDetailView"
 				eventMapper={event => ({
 					...event,
 					title: event.title.replace(/^Convocation: /, ''),
