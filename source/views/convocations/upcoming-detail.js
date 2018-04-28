35985af0954a0fae51ecac3d59fc40ca8a9fa072
Hawken Rives <hawkrives@gmail.com>
2018-04-28 08:23:59 -0500

    start work on the new convo detail view

---
+++ source/views/convocations/upcoming-detail.js
@@ -0,0 +1,148 @@
+// @flow
+import * as React from 'react'
+import {Text, ScrollView, StyleSheet} from 'react-native'
+import {Cell, Section, TableView} from 'react-native-tableview-simple'
+import type {EventType, PoweredBy} from '../calendar/types'
+import type {TopLevelViewPropsType} from '../types'
+import {ShareButton} from '../components/nav-buttons'
+import openUrl from '../components/open-url'
+import {ListFooter} from '../components/list'
+import {ButtonCell} from '../components/cells/button'
+import {
+	getLinksFromEvent,
+	addToCalendar,
+	shareEvent,
+	getTimes,
+} from '../calendar/calendar-util'
+import delay from 'delay'
+
+const styles = StyleSheet.create({
+	chunk: {
+		paddingVertical: 10,
+	},
+})
+
+function MaybeSection({header, content}: {header: string, content: string}) {
+	return content.trim() ? (
+		<Section header={header}>
+			<Cell
+				cellContentView={
+					<Text selectable={true} style={styles.chunk}>
+						{content}
+					</Text>
+				}
+			/>
+		</Section>
+	) : null
+}
+
+function Links({header, event}: {header: string, event: EventType}) {
+	const links = getLinksFromEvent(event)
+
+	return links.length ? (
+		<Section header={header}>
+			{links.map(url => (
+				<Cell
+					key={url}
+					accessory="DisclosureIndicator"
+					onPress={() => openUrl(url)}
+					title={url}
+				/>
+			))}
+		</Section>
+	) : null
+}
+
+const CalendarButton = ({message, disabled, onPress}) => {
+	return (
+		<Section footer={message}>
+			<ButtonCell
+				disabled={disabled}
+				onPress={onPress}
+				title="Add to calendar"
+			/>
+		</Section>
+	)
+}
+
+type Props = TopLevelViewPropsType & {
+	navigation: {
+		state: {params: {event: EventType, poweredBy: ?PoweredBy}},
+	},
+}
+
+type State = {
+	message: string,
+	disabled: boolean,
+}
+
+export class UpcomingConvocationsDetailView extends React.Component<Props, State> {
+	static navigationOptions = ({navigation}: any) => {
+		const {event} = navigation.state.params
+		return {
+			title: event.title,
+			headerRight: <ShareButton onPress={() => shareEvent(event)} />,
+		}
+	}
+
+	state = {
+		message: '',
+		disabled: false,
+	}
+
+	addEvent = async (event: EventType) => {
+		const start = Date.now()
+		this.setState(() => ({message: 'Adding event to calendar…'}))
+
+		// wait 0.5 seconds – if we let it go at normal speed, it feels broken.
+		const elapsed = Date.now() - start
+		if (elapsed < 500) {
+			await delay(500 - elapsed)
+		}
+
+		const result = await addToCalendar(event)
+
+		if (result) {
+			this.setState(() => ({
+				message: 'Event has been added to your calendar',
+				disabled: true,
+			}))
+		} else {
+			this.setState(() => ({
+				message: 'Could not add event to your calendar',
+				disabled: false,
+			}))
+		}
+	}
+
+	onPressButton = () => this.addEvent(this.props.navigation.state.params.event)
+
+	render() {
+		const {event, poweredBy} = this.props.navigation.state.params
+		console.log(event.metadata.reasonId)
+
+		return (
+			<ScrollView>
+				<TableView>
+					<Section header="TEST">
+						<Cell title="test view" />
+					</Section>
+					<MaybeSection content={event.title} header="EVENT" />
+					<MaybeSection content={getTimes(event)} header="TIME" />
+					<MaybeSection content={event.location} header="LOCATION" />
+					<MaybeSection content={event.description} header="DESCRIPTION" />
+					<Links event={event} header="LINKS" />
+					<CalendarButton
+						disabled={this.state.disabled}
+						message={this.state.message}
+						onPress={this.onPressButton}
+					/>
+
+					{poweredBy.title ? (
+						<ListFooter href={poweredBy.href} title={poweredBy.title} />
+					) : null}
+				</TableView>
+			</ScrollView>
+		)
+	}
+}
