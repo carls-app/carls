5a3ede509a631409434786713ba1ac20e5416b31
Hawken Rives <hawkrives@gmail.com>
2018-05-07 22:28:28 -0500

    update carls build version to be normal, not inflated

--- fastlane/lib/versions.rb
+++ fastlane/lib/versions.rb
@@ -49,12 +49,6 @@ def propagate_version(**args)
 
   UI.message "Setting build number to #{build}"
 
-  # android's build number goes way up because we need to exceed the old build
-  # numbers generated for the x86 build.
-  ci_build_num = build
-  build = ((2 * 1048576) + build.to_i).to_s if lane_context[:PLATFORM_NAME] == :android
-  UI.message "Actually setting build number to #{build} because we're on android"
-
   version = "#{version.split('-')[0]}-pre" if should_nightly?
   UI.message "Actually putting #{version} into the binaries (because we're doing a nightly)"
 
