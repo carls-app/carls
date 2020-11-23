51935c19b6f3b72fc4057f4d92ed0c5539de53b3
Drew Volz <volz.drew@gmail.com>
2020-11-23 13:02:47 -0700

    poke again

--- scripts/prepare-third-party.sh
+++ scripts/prepare-third-party.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-pwd
+ls
+ls node_modules
+ls node_modules/react-native/
 cd node_modules/react-native/third-party/glog-0.3.4/ && ../../scripts/ios-configure-glog.sh && cd ../../../../
