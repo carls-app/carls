b13313a0acb4947365a5696faee64e8eff578af3
Drew Volz <volz.drew@gmail.com>
2020-11-23 15:25:33 -0700

    More glog poking

--- scripts/prepare-third-party.sh
+++ scripts/prepare-third-party.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# https://github.com/facebook/react-native/issues/14382
-cd node_modules/react-native/scripts/
-curl https://raw.githubusercontent.com/facebook/react-native/5c53f89dd86160301feee024bce4ce0c89e8c187/scripts/ios-configure-glog.sh > ios-configure-glog.sh
-chmod +x ios-configure-glog.sh
+cd node_modules/react-native/third-party/glog-0.3.4 && ../../scripts/ios-configure-glog.sh && cd ../../../../
