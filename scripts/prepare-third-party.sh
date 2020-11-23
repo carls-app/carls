9b63f3ed3d96e503de929c3221d2c7dd7cebfb71
Drew Volz <volz.drew@gmail.com>
2020-11-23 14:45:44 -0700

    Poke at xcode-select

--- scripts/prepare-third-party.sh
+++ scripts/prepare-third-party.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+sudo xcode-select -s /Applications/Xcode.app
 node_modules/react-native/scripts/ios-install-third-party.sh && cd node_modules/react-native/third-party/glog-0.3.4/ && ../../scripts/ios-configure-glog.sh && cd ../../../../
