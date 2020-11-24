ebb5d37f0ff5aaba79cefb7435db1328b6f9b317
Drew Volz <volz.drew@gmail.com>
2020-11-23 21:03:41 -0700

    More poking at installing in the correct folder

--- scripts/prepare-third-party.sh
+++ scripts/prepare-third-party.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd node_modules/react-native/scripts && ./ios-install-third-party.sh && cd ./third-party/glog-0.3.4 && ./configure && cd ../../../../../
+cd node_modules/react-native && ./scripts/ios-install-third-party.sh && cd ./third-party/glog-0.3.4 && ./configure && cd ../../../../../
