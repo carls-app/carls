494d5d4a8fe185d125269ad45e8d703aa07aafbb
Drew Volz <volz.drew@gmail.com>
2020-11-23 21:41:10 -0700

    Add script twice for debug

--- scripts/prepare-third-party.sh
+++ scripts/prepare-third-party.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cd node_modules/react-native && scripts/ios-install-third-party.sh && third-party/glog-0.3.4/configure && cd ../../
+cd node_modules/react-native && scripts/ios-install-third-party.sh && third-party/glog-0.3.4/configure && cd ../../
