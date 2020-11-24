212a4271465129990d0151597bb62be53fea2d0b
Drew Volz <volz.drew@gmail.com>
2020-11-23 18:54:14 -0700

    Tweak the glog scripts

--- scripts/prepare-third-party.sh
+++ scripts/prepare-third-party.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cd node_modules/react-native/third-party/glog-0.3.4 && ../../scripts/ios-configure-glog.sh && cd ../../../../
+cd node_modules/react-native/scripts && ./ios-install-third-party.sh
+cd .. && ./third-party/glog-0.3.4 && ./configure && cd ../../../../
